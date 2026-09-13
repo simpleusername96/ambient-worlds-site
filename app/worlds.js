741332030e9c25e9a07969f706ed39fa1e3ee010
이병헌 <97590919+simpleusername96@users.noreply.github.com>
2026-09-13 20:24:48 +0900

    Publish the reviewed world registry

--- app/worlds.js
+++ app/worlds.js
@@ -1,14 +1,6 @@
 export const WORLD_ORDER = ["journey", "stillwater"];
 
 export const WORLDS = Object.freeze({
-  meadow: Object.freeze({
-    id: "meadow",
-    label: "Meadow",
-    adapter: "worlds/meadow/adapter.html",
-    fallbackImage: "references/user/2026-09-11-meadow-infinite-world.webp",
-    autoDurationMs: 150_000,
-    defaultCapabilities: Object.freeze({ play: false, sound: false, scenes: false })
-  }),
   journey: Object.freeze({
     id: "journey",
     label: "Journey",
